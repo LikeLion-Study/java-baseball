db22f39a3eaa3580bcce8da0cacaa81b0709f97a
HyunjeLee <cyeclamen07@gmail.com>
2025-04-16 14:42:58 +0900

    feat(): 목표값 생성 기능 구현
    - Random 클래스의 ints 메서드 사용
    - 추후 연산을 위해 목표값을 배열로 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import java.util.Random;
+import java.util.stream.IntStream;
+
 public class Application {
+
+    int[] targetNumber;
+
     public static void main(String[] args) {
-        // TODO: 코드 구현
+
+        Application application = new Application();
+
+        application.createTargetNumber(1, 9, 3);
+
+    }
+
+    private void createTargetNumber(int start, int end, int size) {
+
+        Random random = new Random();
+
+        IntStream intStream = random.ints(start, end + 1).limit(size);
+
+        targetNumber = intStream.toArray();
     }
 }
