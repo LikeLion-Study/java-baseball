a7bad90fb3d0bb13be062b2487438cadf053ad1b
HyunjeLee <cyeclamen07@gmail.com>
2025-04-16 15:37:57 +0900

    refactor(): NUM_SIZE로 기존 size 대체

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,10 @@
 package baseball;
 
-import java.util.Arrays;
 import java.util.Random;
 import java.util.stream.IntStream;
 
 public class Application {
+    static final int NUM_SIZE = 3;
 
     int[] targetNumber;
 
@@ -12,17 +12,17 @@ public class Application {
 
         Application application = new Application();
 
-        application.createTargetNumber(1, 9, 3);
+        application.createTargetNumber(1, 9);
 
         System.out.println("숫자 야구 게임을 시작합니다.");
 
     }
 
-    private void createTargetNumber(int start, int end, int size) {
+    private void createTargetNumber(int start, int end) {
 
         Random random = new Random();
 
-        IntStream intStream = random.ints(start, end + 1).limit(size);
+        IntStream intStream = random.ints(start, end + 1).limit(NUM_SIZE);
 
         targetNumber = intStream.toArray();
     }
